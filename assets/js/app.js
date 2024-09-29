143da335485980c2aecd8f2d55d90542fb66657a
Gold Lee <USER@GoldLee.GoldLee001>
2024-09-29 06:15:21 +0100

    hero

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,21 @@ document.addEventListener('DOMContentLoaded', function() {
         menu.style.display = 'block'
     });
 });
+
+// clock
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+// Initialize the clock immediately
+updateClock();
